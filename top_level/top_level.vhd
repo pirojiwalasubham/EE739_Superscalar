36bccd8286c54bbac1d5e472fdfbaec18ea5f1df
Subham Pirojiwala <pirojiwalasubham@gmail.com>
2019-05-07 01:55:51 +0530

    toplevel started

---
+++ top_level/top_level.vhd
@@ -0,0 +1,51 @@
+library ieee;
+use ieee.numeric_std.all;
+use ieee.std_logic_1164.all;
+
+
+entity top_level is
+		port(clk, reset_bar, Clock_50: in std_logic
+			);
+end entity;
+
+
+architecture behave of top_level is
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end architecture behave;
